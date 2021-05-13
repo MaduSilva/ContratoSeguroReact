acc334172466ceb1c571a118a780296af5774e5c
Maria Eduarda <marriae@hotmail.com>
2021-05-13 16:48:21 -0300

    Footer e Header

---
+++ contratosegurofront/src/components/rodape/rodape.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../rodape/rodape.css'
+
+//imagens 
+import logo from '../../assets/img/logo-brq.png';
+
+export default function Rodape() {
+  return (
+    <div className="Container">
+      <img src={logo} className="footer-logo" alt="logo brq" />
+      <a className="TextFooter1" >© 2021 BRQ</a>
+      <a className="TextFooter2">Todos os direitos reservados 2021.</a>
+      <a href="https://www.brq.com/politica-privacidade/" className="TextFooter3">Politicas de Privacidade</a>
+    </div>
+  );
+}
