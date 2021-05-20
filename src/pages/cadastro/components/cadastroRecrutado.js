0bc78a3938ce05de550aa668b49550dc68928721
Matheus Henrique <matheus.barboza.henrique@gmail.com>
2021-05-20 16:25:45 -0300

    aplicação api

--- src/pages/cadastro/components/cadastroRecrutado.js
+++ src/pages/cadastro/components/cadastroRecrutado.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  Form , useFormik , MyFormik } from 'formik';
+import { Formik, Form } from 'formik';
 import { TextField } from './textField'
 import '../cadastroRecrutado/index.css'
 import * as Yup from 'yup';
@@ -24,40 +24,39 @@ export const CadastroRec = () => {
             .max(15, 'Mais de 15 carcteries no CPF')
             .required('senha is Required'),
     })
-
-    const formik = useFormik({
-        initialValues : {
-            nome: '',
-            email: '',
-            cpf: '',
-            senha: '',
-        },
-        onsubmit : values => {
-            alert(values); 
-        }
-    })
-
-
     return(
         
-            
+        <Formik
+            initialValues={{
+                nome: '',
+                email: '',
+                cpf: '',
+                senha: '',
+            }}
+            validationSchema={validate}
+            onSubmit={values => {
+                console.log(values)
+            }}
+        >
+            {formik => (
                 <div>
                     <h1 className="my-3 font-weight-bold-display-7">Cadastro de Recrutado </h1>
                     
-                    <Form className='imputs' onsubmit={formik.handleSubmit}>
-                        <TextField label="Nome" name="nome" type="text" value={formik.values.nome} onChange={formik.handleChange} required/>
-                        <TextField label="Email" name="email" type="email" value={formik.values.email} onChange={formik.handleChange} required/>
-                        <TextField label="CPF" name="cpf" type="cpf" value={formik.values.cpf}  onChange={formik.handleChange} required />
-                        <TextField label="Senha" name="senha" type="password" value={formik.values.senha}  onChange={formik.handleChange} required />
+                    <Form className='imputs'>
+                        <TextField label="Nome" name="nome" type="text" />
+                        <TextField label="Email" name="email" type="email" />
+                        <TextField label="CPF" name="cpf" type="cpf" />
+                        <TextField label="Senha" name="senha" type="password" />
                         
                         <div className="botoes">
-                        <button className="btn btn-dark mt-3"  >Cancelar</button>
+                        <button className="btn btn-dark mt-3" type="reset" >Cancelar</button>
                         <button className="btn btn-primary mt-3" type="submit" value='Submit' >Enviar</button>
                             
                         </div>
                     </Form>
                 </div>
-         
+            )}
+        </Formik>
             
         
     )
