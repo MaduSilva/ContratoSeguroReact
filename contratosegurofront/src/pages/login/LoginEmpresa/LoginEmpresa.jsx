22fc031fe67bc7c6cbe03e4b4c87edd4823d39e4
Yan Trindade <yan.trindade@portalsesisp.org.br>
2021-05-20 17:53:58 +0000

    LoginEmpresa Pronto

--- contratosegurofront/src/pages/login/LoginEmpresa/LoginEmpresa.jsx
+++ contratosegurofront/src/pages/login/LoginEmpresa/LoginEmpresa.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
 import Rodape from '../../../Components/Rodape/rodape';
 import Menu from '../../../Components/Menu/menu';
@@ -6,11 +6,55 @@ import './loginEmpresa.css';
 import Foguete from '../../../assets/img/fuguete.png';
 import Logo from '../../../assets/img/logo2.png';
 import Seta from '../../../assets/img/seta.png';
+import { useHistory } from 'react-router';
 
 
 
 
 const LoginEmpresa = () => {
+  const history = useHistory();
+     const[email , setEmail] = useState('');
+     const[cnpj , setCnpj] = useState('');
+     const[senha, setSenha] = useState('');
+
+     const logar = (event) => {
+      event.preventDefault();
+
+
+      fetch('http://localhost:5000/api/account/company/signin/empresa',{ 
+         method : 'POST',
+         body : JSON.stringify({
+
+            email: email,
+            senha: senha,
+            cnpj : cnpj,
+
+         }),
+         headers : {
+           'content-type' : 'application/json'
+         }
+         
+
+
+      
+        })
+        .then(response => {
+          if(response.ok === true){
+            return response.json();
+          }
+          alert('Dados Invalidos');
+          console.log(response);
+        })
+        .then(data =>{
+
+          localStorage.setItem('ChaveSecretaContratoSeguro', data.token);
+          
+          
+          history.push('/home')
+        })
+        .catch(err => console.error(err));
+      
+    }
     return (
      <div class="body">
       <Menu/>
@@ -31,21 +75,22 @@ const LoginEmpresa = () => {
       <a class="vol"> <b>Voltar</b></a>
       </div>
      <div>
+     <Form>
      <img src={Logo} class="logo-a" alt="logo" />
      
      
      <Form.Group>
-     <Form.Control class="form-control" size="lg" type="text" placeholder="CNPJ" />
+     <Form.Control class="form-control" size="lg" type="email" onChange={ event => setEmail(event.target.value)} value={email} placeholder="Email Comercial" />
      <br />
-     <Form.Control class="form-control" type="text" placeholder="Email Comercial" />
+     <Form.Control class="form-control" type="email" value={cnpj} onChange={ event => setCnpj(event.target.value)} placeholder="CNPJ" />
      <br />
-     <Form.Control class="form-control" size="sm" type="text" placeholder="Senha" />
-     <Button class="Button"variant="primary" size="lg" active>
+     <Form.Control class="form-control" size="sm" type="password" value={senha} onChange={ event => setSenha(event.target.value)} placeholder="Senha" />
+     <Button class="Button"variant="primary" onClick={ event => logar(event)}  size="lg" active>
       Entrar
      </Button>{' '}
      <a class="esq"><b>Esqueci a senha!</b></a>
      </Form.Group>
-     
+     </Form>
      </div>
      </div>
      </div>
