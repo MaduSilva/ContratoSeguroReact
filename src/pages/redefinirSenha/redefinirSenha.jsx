571b0314b838c0696cf277fac58502fa6a56b063
Yan Ad'Vincula Trindade <61596255+Yan-91@users.noreply.github.com>
2021-06-24 17:01:35 -0300

    responsivo e possivelmente funcional

--- src/pages/redefinirSenha/redefinirSenha.jsx
+++ src/pages/redefinirSenha/redefinirSenha.jsx
@@ -10,6 +10,7 @@ import Seta2 from '../../assets/img/seta2.png';
 import Foguete from '../../assets/img/fuguete.png'
 import { useFormik } from 'formik';
 import { useToasts } from 'react-toast-notifications';
+import * as Yup from 'yup';
 
 import esquecisenha from '../../assets/img/esquecisenha.png'
 
@@ -17,19 +18,27 @@ import esquecisenha from '../../assets/img/esquecisenha.png'
 const RedefinirSenha = () => {
 
   const history = useHistory();
+  
+  
 
-  const data = {
-    email: this.email
+  const initialValues = {
+    email: '',
   };
-  axios.post('forgot', data).then(
-    res=>{
-      console.log(res)
-    }
-  ).catch(
-    err=>{
-      console.log(err);
-    }
-  )
+
+  const formSchema = Yup.object().shape({
+    email: Yup.string().email('E-mail inválido').required('Obrigatório'),
+  });
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        formik.setSubmitting(false);
+      }, 3000);
+    },
+  });
 
 
   return (
@@ -37,37 +46,40 @@ const RedefinirSenha = () => {
     <div class="body">
       <Menu />
       <Container>
+        <div class="redefinir">
         <div class="FundoInfo">
           <h1>Redefinir senha</h1>
-          <img src={esquecisenha} className="foguete-logo" alt="foguete" />
+          <img src={esquecisenha} className="foguete-logo1" alt="foguete" />
         </div>
-        <div Class="FundoForm">
-          <div class="InputsForm">
-            <div>
+        <div Class="FundForm" >
+          <div class="InputForms" >
+            <div >
               
               <img class="setaEmpresa" src={Seta2} alt="seta" />
             </div>
             <div >
-              <Form className='form-signin' onSubmit={formik.handleSubmit}  >
+              <Form className='form-signin' onSubmit={formik.handleSubmit}>
                 
                 <img src={Logo} class="logo-a" alt="logo" />
 
                 
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Control style={{backgroundColor: 'white', width: '200px', marginLeft: '49px'}} type="email" placeholder="Email" name="email" onChange={formik.handleChange} value={formik.values.email} required />
+                <Form.Group controlId="formBasicEmail" >
+                  <Form.Control style={{backgroundColor: 'white', width: '200px'}}  id="email" placeholder="Email" onChange={formik.handleChange} value={formik.values.email} error={formik.touched.email && Boolean(formik.errors.email)} helperText={formik.touched.email && formik.errors.email}  />
                 </Form.Group>
 
                
 
 
-                <Button className="ButtonSignIn" variant="primary" type="submit" disabled={formik.isSubmitting}>
+                <Button className="ButtonSignIng" variant="primary" type="submit" disabled={formik.isSubmitting} >
                   Confirmar
                     </Button>
                 <br /><br />
                 
+                
               </Form>
             </div>
           </div>
+        </div>
         </div>
       </Container>
       <Rodape />
